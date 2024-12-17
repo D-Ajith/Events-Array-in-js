713247d9333df5c5b98cd4378cc827b6e44641bd
D-Ajith <doddi.ajith2003@gmail.com>
2024-12-17 19:17:25 +0530

    >Js events and arrays

---
+++ event.js
@@ -0,0 +1,137 @@
+// let a=document.getElementById("image ele")
+// function image(){
+//     a.src="https://static.indiatvnews.com/ins-web/images/anushka-3082-1541034692.jpg"
+// }
+
+// let b=document.getElementById("charan")
+// function getcharan(){
+//     b.src="https://i.pinimg.com/736x/3c/28/2c/3c282cf8d38bff6560b403bbceeaba77.jpg"
+// }
+
+// let c=document.getElementById("ntr")
+// c.onclick=function(){
+//     c.src="https://upload.wikimedia.org/wikipedia/commons/5/5f/Alia_Bhatt%2C_Chennai_%28cropped_2%29.jpg"
+// }
+
+// let d=document.getElementById("vd")
+// d.onclick=function(){
+//     d.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReStg38JjAyiET9tvFdLTvit5R__TMiJCafw&s"
+// }
+
+// let btn1=document.getElementById("dbl")
+// btn1.ondblclick=function(){
+//     btn1.style.height="250px"
+//     btn1.style.width="250px"
+//     btn1.style.backgroundColor="red"
+//     btn1.style.color="white"
+// }
+
+// let btn2=document.getElementById("hover")
+// btn2.onmouseover=function(){
+//     btn2.style.height="250px"
+//     btn2.style.width="250px"
+//     btn2.style.backgroundColor="purple"
+//     btn2.style.color="white"
+// }
+
+// let btn3=document.getElementById("ck")
+// btn3.onclick=function(){
+//     btn3.style.height="250px"
+//     btn3.style.width="250px"
+//     btn3.style.backgroundColor="blue"
+//     btn3.style.color="white"
+// }
+
+
+// let inpu=document.querySelector("input")
+// inpu.onkeypress=function(){
+//     document.body.style.backgroundColor="red"
+// }
+
+
+// let a=document.getElementById("charan")
+// function getcharan(){
+//     a.src="https://img.etimg.com/thumb/width-1200,height-1200,imgsize-68134,resizemode-75,msid-116063773/magazines/panache/samantha-ruth-prabhu-enjoys-a-most-beautiful-evening-after-ex-husband-naga-chaitanyas-wedding.jpg"
+// }
+
+// let b=document.getElementById("ntr")
+// function getntr(){
+//     b.src="https://images.indianexpress.com/2023/07/alia-bhatt-14.jpg"
+// }
+
+// let c=document.getElementById("murunal")
+// c.ondblclick=function(){
+//     c.src="https://images.hindustantimes.com/img/2022/04/09/550x309/05e867d8-b82b-11ec-a4f3-fc37f02059fa_1649532004301.jpg"
+// }
+
+// let btn1=document.getElementById("hovered")
+// btn1.onmouseover=function(){
+//     btn1.style.width="250px"
+//     btn1.style.height="250px"
+//     btn1.style.backgroundColor="red"
+//     btn1.style.color="white"
+// }
+
+// let btn2=document.getElementById("clicked")
+// btn2.onclick=function(){
+//     btn2.style.width="250px"
+//     btn2.style.height="250px"
+//     btn2.style.backgroundColor="purple"
+//     btn2.style.color="white"
+// }
+
+// let btn3=document.getElementById("double click")
+// btn3.ondblclick=function(){
+//     btn3.style.width="250px"
+//     btn3.style.height="250px"
+//     btn3.style.backgroundColor="green"
+//     btn3.style.color="white"
+// }
+
+// let inpu=document.querySelector("input")
+// inpu.onkeypress=function(){
+//     document.body.style.backgroundColor="red"
+// }
+
+// let ptag=document.querySelectorAll("p")
+// // ptag.onmouseover=function(){
+// for(i=0;i<ptag.length;i++){
+//     ptag[i].style.backgroundColor="red"
+// }}
+
+
+let a=document.getElementById("charan")
+function getcharan(){
+    a.src="https://media.assettype.com/freepressjournal/2023-10/19067b70-8ff0-43c0-88bc-f53e35ec0ed0/aliabhatt.jpg"
+}
+
+let b=document.getElementById("vd")
+b.onmouseover=function(){
+    b.src="https://c.ndtvimg.com/2024-04/8cfhv6q8_rashmika-mandanna_625x300_05_April_24.jpg?im=FaceCrop,algorithm=dnn,width=1200,height=738"
+}
+
+let c=document.querySelector("input")
+c.onkeypress=function(){
+    document.body.style.backgroundColor="red"
+}
+
+let d=document.getElementById("hovered")
+d.onmouseover=function(){
+         d.style.width="250px"
+          d.style.height="250px"
+           d.style.backgroundColor="red"
+}
+
+let e=document.getElementById("clicked")
+e.onclick=function(){
+         e.style.width="250px"
+          e.style.height="250px"
+           e.style.backgroundColor="purple"
+}
+
+let f=document.getElementById("dbled")
+f.ondblclick=function(){
+         f.style.width="250px"
+          f.style.height="250px"
+           f.style.backgroundColor="green"
+}
